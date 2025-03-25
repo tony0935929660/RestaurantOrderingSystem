eb620bdd0925cc5960a7b434d9aace32fc77a353
jflin <tony0970941055@gmail.com>
2025-03-26 00:08:59 +0800

    feat: new dbcontext

---
+++ backend/Models/RestaurantOrderingSystemContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Models;
+
+public partial class RestaurantOrderingSystemContext : DbContext
+{
+    public RestaurantOrderingSystemContext()
+    {
+    }
+
+    public RestaurantOrderingSystemContext(DbContextOptions<RestaurantOrderingSystemContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
